d094c9c4048be5cfab07b0799179ebacb3ec8ccb
Balajigunasekaran02 <balajigunasekaran02@gmail.com>
2023-09-11 17:46:54 +0500

    added home layout and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <Footer />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 export default App;
